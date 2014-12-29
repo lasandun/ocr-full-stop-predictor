fb02b315e9267e562e76bbf727e29135a14c8dd3
lasandun <lasadun@gmail.com>
2014-12-29 20:11:20 +0530

    Test files added

---
+++ src/ocr/full/stop/predictor/OcrFullStopPredictor.java
@@ -0,0 +1,15 @@
+package ocr.full.stop.predictor;
+
+/**
+ *
+ * @author lahiru
+ */
+public class OcrFullStopPredictor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
